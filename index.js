071879b06a43212b00f77bf21fe2d424fbe255ca
olegchursin <olegchursin@gmail.com>
2018-01-30 17:54:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  return array.filter (function (driver) {return driver.toLowerCase() === string.toLowerCase()})
+};
+
+function fuzzyMatch(array, string) {
+  return array.filter (function (driver) {return driver.toLowerCase().slice(0,string.length) === string.toLowerCase()})
+};
+
+function matchName(array, string) {
+  return array.filter (function (driver) {return driver.name.toLowerCase() === string.toLowerCase()})
+};
